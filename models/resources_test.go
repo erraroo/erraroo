3eba73a415f596ff6dd1cb4e20754eb45a35bc0c
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-06-13 19:47:44 -0700

    just grab some thigns from the net and cache them locally

--- models/resources_test.go
+++ models/resources_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"testing"
 
@@ -14,22 +15,38 @@ type mockResourceGetter struct{}
 
 func (m *mockResourceGetter) Get(url string) (io.ReadCloser, error) {
 	filename := fmt.Sprintf("../test/fixtures/%x", md5.Sum([]byte(url)))
+	_, err := os.Open(filename)
+	if err != nil {
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		f, err := os.Create(filename)
+		if err != nil {
+			return nil, err
+		}
+
+		io.Copy(f, resp.Body)
+	}
+
 	return os.Open(filename)
 }
 
 func TestResourceStore(t *testing.T) {
-	sourceURL := "http://erraroo.com/assets/erraroo-50c293f8d38851c3fa2c04bb62fb8e3f.js"
+	sourceURL := "https://d16vxe267myqks.cloudfront.net/assets/erraroo-a97979fee2e8d42dc9a281a233f895b2.js"
 	resources := &resourcesStore{&mockResourceGetter{}}
 
 	resource, err := resources.FindByURL(sourceURL)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, resource.SourceMap)
 
-	//context := resource.Context(536, 15)
-	//assert.Equal(t, "        throw new Error('i threw an error');", context.ContextLine)
-	//assert.Equal(t, 17, context.OrigLine)
-	//assert.Equal(t, 1, context.OrigColumn)
-	//assert.Equal(t, "erraroo/routes/application.js", context.OrigFile)
+	context := resource.Context(1, 13983)
+	assert.Equal(t, "        throw new Error('i threw an error' + Math.random());", context.ContextLine)
+	assert.Equal(t, 17, context.OrigLine)
+	assert.Equal(t, 0, context.OrigColumn)
+	assert.Equal(t, "erraroo/routes/application.js", context.OrigFile)
 }
 
 func TestResourcesWithoutSourceMap(t *testing.T) {
