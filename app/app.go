cd21ff9ab4aead5a64f146ce56976bb13c4eb4ee
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-06-14 16:54:30 -0700

    gzip middleware

--- app/app.go
+++ app/app.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"compress/gzip"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -216,7 +219,8 @@ func New() *App {
 
 	a.HTTPServer = alice.New(
 		c.Handler,
-		loggingHandler,
+		loggingMiddleware,
+		gzipMiddleware,
 	).Then(a.Router)
 
 	a.setupQueue()
@@ -262,11 +266,33 @@ func (l *loggedResponse) WriteHeader(status int) {
 	l.ResponseWriter.WriteHeader(status)
 }
 
-func loggingHandler(h http.Handler) http.Handler {
+func loggingMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		ww := &loggedResponse{w, -1}
 		h.ServeHTTP(ww, r)
 		logger.Info("request", "method", r.Method, "url", r.URL.Path, "runtime", time.Since(start), "status", ww.status)
+	})
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func gzipMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			handler.ServeHTTP(w, r)
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzw := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+		handler.ServeHTTP(gzw, r)
 	})
 }
