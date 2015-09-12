a7bd1032cb8464c12cb6d49843527789d4c51fe3
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-09-11 19:57:11 -0700

    this should fix rate lmit tests

--- models/rate_limit_notifications.go
+++ models/rate_limit_notifications.go
@@ -21,17 +21,23 @@ type rateLimitNotifcationsStore struct{ *Store }
 
 func (store *rateLimitNotifcationsStore) WasRecentlyNotified(account *Account) (bool, error) {
 	var count int
-	err := store.DB.Table("rate_limit_notifications").Where("created_at <= now_utc() - interval '30 minutes'").Count(&count).Error
+	err := store.DB.
+		Table("rate_limit_notifications").
+		Where("account_id = ?", account.ID).
+		Where("created_at >= now_utc() - interval '30 minutes'").Count(&count).Error
+
 	return count > 0, err
 }
 
 func (store *rateLimitNotifcationsStore) Insert(account *Account) error {
-	notification := &RateLimitNotification{
+	n := &RateLimitNotification{
 		AccountID: account.ID,
 		CreatedAt: time.Now().UTC(),
 	}
 
-	if err := store.DB.Create(notification).Error; err != nil {
+	query := "insert into rate_limit_notifications (account_id, created_at) values($1, now_utc()) returning id, created_at"
+	err := store.QueryRow(query, account.ID).Scan(&n.ID, &n.CreatedAt)
+	if err != nil {
 		logger.Error("creating rate limit notifcation", "account", account.ID, "err", err)
 		return err
 	}
